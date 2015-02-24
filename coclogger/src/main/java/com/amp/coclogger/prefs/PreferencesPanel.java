c183a71f80e1f8d3385e25f2680cb98836df1f68
apritchard <pritchard.alex@gmail.com>
2015-02-24 15:40:37 -0600

    Add support for directory picking to preferences

--- coclogger/src/main/java/com/amp/coclogger/prefs/PreferencesPanel.java
+++ coclogger/src/main/java/com/amp/coclogger/prefs/PreferencesPanel.java
@@ -1,7 +1,9 @@
 package com.amp.coclogger.prefs;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,6 +11,7 @@ import java.util.prefs.Preferences;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -34,9 +37,9 @@ public class PreferencesPanel extends JPanel {
 			case BOOLEAN:
 				addCheckbox(prefName);
 				break;
-//			case DIRECTORY:
-//				addDirectory(prefName);
-//				break;
+			case DIRECTORY:
+				addDirectory(prefName);
+				break;
 			case INTEGER:
 				addInteger(prefName);
 				break;
@@ -94,6 +97,50 @@ public class PreferencesPanel extends JPanel {
 		textField.setText("" + prefs.getInt(prefName.getPathName(),  0));
 		add(textField, "width 60px, wrap");
 		integerFields.put(textField, prefName.getPathName());
+	}
+	
+	private void addDirectory(final PrefName prefName){
+		add(new JLabel(prefName.getPathName()));
+		
+		final JTextField textField = new JTextField();
+		final Component chooserParent = this;
+		
+		textField.setText(prefs.get(prefName.getPathName(), ""));
+		textField.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chooseDirectory(textField, prefName, chooserParent);
+			}
+		});		
+		add(textField, "width 300px");
+		
+		JButton browseButton = new JButton("Browse");
+		browseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chooseDirectory(textField, prefName, chooserParent);
+			}
+		});
+		add(browseButton, "wrap");
+	}
+	
+	private void chooseDirectory(JTextField textField, PrefName prefName, Component chooserParent){
+		JFileChooser chooser = new JFileChooser();
+		String previousDirectory = textField.getText();
+		if(!previousDirectory.isEmpty()){
+			chooser.setCurrentDirectory(new File(previousDirectory));
+		} else {
+			chooser.setCurrentDirectory(new File("."));
+		}
+		chooser.setDialogTitle("Select " + prefName.getPathName() + " Directory");
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setAcceptAllFileFilterUsed(false);
+		
+		if (chooser.showOpenDialog(chooserParent) == JFileChooser.APPROVE_OPTION){
+			textField.setText(chooser.getSelectedFile().getAbsolutePath());
+		}
+		
+
 	}
 	
 }
